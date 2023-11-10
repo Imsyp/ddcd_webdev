7547fdceaa095628d6c3dac83996a1d804b79d3b
Imsyp <tkddud386@gmail.com>
2023-11-10 18:39:29 +0900

    20231110

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
+var url = require('url');
 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    console.log(queryData.id);  //queryData에서 key값에 따른 value를 얻을 수 있음
+    if(_url == '/'){
+      title = 'Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
       response.end();
       return;
     }
     response.writeHead(200);
-    //console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    var template = `
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>  //WEB 버튼 눌렀을 때, if(_url = '/')의 조건문에 걸리도록
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li> //해당 페이지의 주소에서 queryData 부분 id의 value값을 바꿔줌
+    <li><a href="/?id=CSS">CSS</a></li> //해당 페이지의 주소에서 queryData 부분 id의 value값을 바꿔줌
+    <li><a href="/?id=JavaScript">JavaScript</a></li> //해당 페이지의 주소에서 queryData 부분 id의 value값을 바꿔줌
+  </ol>
+  <h2>${title}</h2>
+  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+  <img src="coding.jpg" width="100%">
+  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+  </p>
+</body>
+</html>
+    `;
+    response.end(template);
 
 });
 app.listen(3000);
--- syntax/variable.js
+++ syntax/variable.js
@@ -2,3 +2,6 @@ var a = 1;
 console.log(a);
 a = 2;
 console.log(a);
+
+var letter = 'meaningless string'; 
+console.log(letter);
