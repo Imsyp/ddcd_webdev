e305cb0b4880cda9307de4324e318fa391b334ce
Imsyp <tkddud386@gmail.com>
2023-11-16 20:56:37 +0900

    20231116

--- main.js
+++ main.js
@@ -5,39 +5,66 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);  
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+    var pathname = url.parse(_url, true).pathname
+  
+
+    if(pathname === '/') {
+        if(queryData.id === undefined) {
+
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>  
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li> 
+                <li><a href="/?id=CSS">CSS</a></li> 
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        } else {
+          var title = queryData.id;
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>  
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li> 
+                <li><a href="/?id=CSS">CSS</a></li> 
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        }
+    } else {
       response.writeHead(404);
-      response.end();
-      return;
+      response.end('Not found')
     }
-    response.writeHead(200);
-    //data 경로에 있는 파일 중 queryData.id의 이름을 가진 파일이 description에 담김
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>  
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li> 
-          <li><a href="/?id=CSS">CSS</a></li> 
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
 });
 app.listen(3000);
