6d1162696bc5bff354c6533d08168625c3d6e948
Imsyp <tkddud386@gmail.com>
2023-11-23 19:04:51 +0900

    20231123

---
+++ syntax/object2.js
@@ -0,0 +1,16 @@
+//자바 스크립트에서는 함수를 변수에 저장할 수 있다.
+var f = function() {
+    console.log(1 + 1);
+    console.log(1 + 2);
+}
+
+//배열에 함수를 담을 수 있다.
+var a = [f];
+a[0]();
+
+//객체에 함수를 담을 수 있다.
+var o = {
+    func:f
+}
+o.func();
+
